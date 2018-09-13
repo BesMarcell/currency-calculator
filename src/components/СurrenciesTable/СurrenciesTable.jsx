7bf3441c16d18c2dabecbe48a4d206f05efd1d4d
Mikalai Snapko <mikalai.snapko@instinctools.ru>
2018-09-13 11:05:10 +0300

    СurrenciesTable

--- src/components/СurrenciesTable/СurrenciesTable.jsx
+++ src/components/СurrenciesTable/СurrenciesTable.jsx
@@ -1,11 +1,42 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import theme from './theme.css';
 
 class СurrenciesTable extends PureComponent {
+  static propTypes = {
+    currencies: PropTypes.array.isRequired,
+  };
+
   render() {
     return (
       <div className={theme.сurrenciesTable}>
-        Table
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Currency_code</TableCell>
+              <TableCell>Buying rate</TableCell>
+              <TableCell>Selling rate</TableCell>
+              <TableCell>Median rate</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.currencies.map(row => (
+              <TableRow key={row.currency_code}>
+                <TableCell component="th" scope="row">
+                  {row.currency_code}
+                </TableCell>
+                <TableCell>{row.buying_rate}</TableCell>
+                <TableCell>{row.selling_rate}</TableCell>
+                <TableCell>{row.median_rate}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     );
   }
